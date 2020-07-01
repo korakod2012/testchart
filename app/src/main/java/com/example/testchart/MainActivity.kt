31d500fe1176a85367b8ec1b11460e3baef56786
blackip <kkoodd2012@gmail.com>
2020-07-01 12:03:26 +0700

    able click to random weight and plot into graph one by one

--- app/src/main/java/com/example/testchart/MainActivity.kt
+++ app/src/main/java/com/example/testchart/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.testchart
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.YAxis.AxisDependency
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,36 +17,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnAddWeight = findViewById<Button>(R.id.btn_PlotWeight)
+        setupChart()
+
+        btnAddWeight.setOnClickListener {
+            addEntry()
+        }
+    }
+
+    private fun addEntry(){
+
+        //get line data from old
+        val data: LineData? = lineChart.data
+
+        if ( data != null){
+            var set = data.getDataSetByIndex(0)
+
+            data.addEntry(
+                Entry(
+                    set.entryCount.toFloat(),
+                    (Math.random() * 40).toFloat() + 30f
+                ), 0
+            )
+            data.notifyDataChanged()
+
+            // let the chart know it's data has changed
+            lineChart.notifyDataSetChanged()
+
+            // move to the latest entry
+            lineChart.moveViewToX(data.entryCount.toFloat())
+
+        }
+    }
+
+    private fun setupChart(){
         val entries = ArrayList<Entry>()
-        entries.add(Entry(1f, 75f))
-        entries.add(Entry(2f, 79f))
-        entries.add(Entry(3f, 77f))
-        entries.add(Entry(4f, 77f))
-        entries.add(Entry(5f, 78f))
-        entries.add(Entry(6f, 75f))
-        entries.add(Entry(7f, 79f))
-        entries.add(Entry(8f, 77f))
-        entries.add(Entry(9f, 77f))
-        entries.add(Entry(10f, 78f))
-
-
-
-        val vl = LineDataSet(entries, "Weight")
-        vl.setDrawValues(false)
-        vl.setDrawFilled(true)
-        vl.lineWidth = 3f
-        vl.fillColor = R.color.gray
-        vl.fillAlpha = R.color.red
-
+        val linedataSet = LineDataSet(entries, "Weight")
+        linedataSet.setDrawValues(false)
+        linedataSet.setDrawFilled(true)
+        linedataSet.lineWidth = 3f
+        linedataSet.fillColor = R.color.gray
+        linedataSet.fillAlpha = R.color.red
         lineChart.xAxis.labelRotationAngle = 0f
-
         lineChart.xAxis.isEnabled = false
-        lineChart.data = LineData(vl)
+        lineChart.data = LineData(linedataSet)
         lineChart.axisRight.isEnabled = false
-        //lineChart.xAxis.axisMaximum = 10F
         lineChart.setTouchEnabled(true)
         lineChart.description.text = "Days"
         lineChart.animateX(1800, Easing.EaseInExpo)
-
     }
+
 }
